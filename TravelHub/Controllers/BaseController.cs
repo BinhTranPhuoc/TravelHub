3d0e49d48bf5a93cbf3c4a853733e81a0eef1e5a
Phước Bình <binh-tp@zenkenplanet-vn.com>
2019-05-14 16:32:39 +0700

    add model of paging

---
+++ TravelHub/Controllers/BaseController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TravelHub.Models.Paging;
+
+namespace TravelHub.Controllers
+{
+    public class BaseController : Controllers
+    {
+        public ActionResult Paging(PagingModel pagingModel)
+        {
+
+        }
+    }
+}
